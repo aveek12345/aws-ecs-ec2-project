1d8471d3ae859f5b764dfea904a9a738bb2cf264
aveek12345 <86606545+aveek12345@users.noreply.github.com>
2021-07-23 18:30:03 +0530

    Update template_file.tf

--- template_file.tf
+++ template_file.tf
@@ -1,6 +1,6 @@
 data "template_file" "task_definition_template" {
     template = file("task_definition.json.tpl")
     vars = {
-      REPOSITORY_URL = replace(aws_ecr_repository.worker.repository_url, "https://", "")
+      REPOSITORY_URL = 787032487300.dkr.ecr.us-east-1.amazonaws.com/worker
     }
 }
