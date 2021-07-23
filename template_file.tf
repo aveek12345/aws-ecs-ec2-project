d19da847228c2b0b2bc239b10885d70edca4354e
aveek12345 <86606545+aveek12345@users.noreply.github.com>
2021-07-23 18:48:34 +0530

    Update template_file.tf

--- template_file.tf
+++ template_file.tf
@@ -1,6 +1,6 @@
 data "template_file" "task_definition_template" {
     template = file("task_definition.json.tpl")
     vars = {
-      REPOSITORY_URL = https://787032487300.dkr.ecr.us-east-1.amazonaws.com/worker
+      REPOSITORY_URL = "https://787032487300.dkr.ecr.us-east-1.amazonaws.com/worker"
     }
 }
