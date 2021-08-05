26aa6cbc0bbba90382784a4a8d70cf808bccd2b2
aveek12345 <86606545+aveek12345@users.noreply.github.com>
2021-08-05 09:42:57 +0530

    Update template_file.tf

--- template_file.tf
+++ template_file.tf
@@ -1,6 +1,7 @@
+#Mention the url of the ECR repository in  REPOSITORY_URL
 data "template_file" "task_definition_template" {
     template = file("task_definition.json.tpl")
     vars = {
-      REPOSITORY_URL = "https://787032487300.dkr.ecr.us-east-1.amazonaws.com/worker"
+      REPOSITORY_URL = ""
     }
 }
